6b39c659a633db8ec12dd6a98eedfb0a510a6a2c
matt476 <71316100+matt476@users.noreply.github.com>
2020-11-08 19:23:40 +0200

    update: zoom on table row click, only when layer is showing

--- Webmap/myScripts.js
+++ Webmap/myScripts.js
@@ -76,56 +76,88 @@ function resetHighlight(e){
 function zoomToFeature(e){
 		map.fitBounds(e.target.getBounds());
     }
-
-	//zoom to catchment on table click
+//zoom to catchment on table click
 	function mamhospharm(){
-		map.setView([-25.71325,28.37463],15);
+		if(map.hasLayer(CL8)){
+			map.setView([-25.71325,28.37463],15);
+		}
     }
 	function mamwestc(){
-		map.setView([-25.71423,28.34883],15);
+		if(map.hasLayer(CL11)){
+			map.setView([-25.71423,28.34883],15);
+		}
     }
 	function AMEp(){
-		map.setView([-25.70892,28.35752],15);
+		if(map.hasLayer(CL10)){
+			map.setView([-25.70892,28.35752],15);
+		}
     }
 	function Stanza1b(){
-		map.setView([-25.70596,28.39407],15);
+		if(map.hasLayer(CL4)){
+			map.setView([-25.70596,28.39407],15);
+		}
     }
 	function S2(){
-		map.setView([-25.71443,28.41557],15);
+		if(map.hasLayer(CL5)){
+			map.setView([-25.71443,28.41557],15);
+		}
     }
 	function phah_c(){
-		map.setView([-25.722,28.38664],15);
+		if(map.hasLayer(CL13)){
+			map.setView([-25.722,28.38664],15);
+		}
     }
 	function hospital(){
-		map.setView([-25.7042,28.41438],15);
+		if(map.hasLayer(CL2)){
+			map.setView([-25.7042,28.41438],15);
+		}
     }
 	function mkp(){
-		map.setView([-25.72179,28.38993],15);
+		if(map.hasLayer(CL14)){
+			map.setView([-25.72179,28.38993],15);
+		}
     }
 	function tshp(){
-		map.setView([-25.70896,28.38318],15);
+		if(map.hasLayer(CL6)){
+			map.setView([-25.70896,28.38318],15);
+		}
     }
 	function mec(){
-		map.setView([-25.72257,28.41587],15);
+		if(map.hasLayer(CL15)){
+			map.setView([-25.72257,28.41587],15);
+		}
     }
 	function lusc(){
-		map.setView([-25.72247,28.43754],15);
+		if(map.hasLayer(CL16)){
+			map.setView([-25.72247,28.43754],15);
+		}
     }
 	function holc(){
-		map.setView([-25.70401,28.37548],15);
+		if(map.hasLayer(CL7)){
+			map.setView([-25.70401,28.37548],15);
+		}
     }
 	function dpmm(){
-		map.setView([-25.70657,28.42283],15);
+		if(map.hasLayer(CL3)){
+			map.setView([-25.70657,28.42283],15);
+		}
     }
 	function khutp(){
-		map.setView([-25.71454,28.3747],15);
+		if(map.hasLayer(CL9)){
+			map.setView([-25.71454,28.3747],15);
+		}
     }
 	function mvp(){
-		map.setView([-25.70095,28.42238],15);
+		if(map.hasLayer(CL1)){
+			map.setView([-25.70095,28.42238],15);
+		}
     }
 	function mamelodihospital(){
-		map.setView([-25.72021,28.3691],15);
+		if(map.hasLayer(CL12)){
+			map.setView([-25.72021,28.3691],15);
+		}
     }
+	
 //make variable for popup style: WARDS
 var popup = L.popup({className: "custom-popup"});
 
