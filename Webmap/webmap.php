cdd605de5496c2c498929e665ba9a8bfa4409a72
Nicholas De Kock <u17198021@tuks.co.za>
2020-11-09 22:42:29 +0200

    changed to dynamic resource links

--- Webmap/webmap.php
+++ Webmap/webmap.php
@@ -6,14 +6,14 @@
     <link rel="icon" href="https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap_Images/Globe.png"><meta charset="utf-8" />
     <!--Linking Stylesheets--->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
-    <link rel="stylesheet" href="../Webmap/L.Control.Layers.Tree.css">
-    <link rel="stylesheet" href="../Webmap/cssStyle.css" /> <!-- coordinate style -->
+    <link rel="stylesheet" href="https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap/L.Control.Layers.Tree.css">
+    <link rel="stylesheet" href="https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap/cssStyle.css" /> <!-- coordinate style -->
     <!--Linking Scripts--->
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="crossorigin=""></script>
-    <script src="../Webmap/L.Control.Layers.Tree.js"></script>
+    <script src="https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap/L.Control.Layers.Tree.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="table2csv.js"></script>
+    <script src="https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap/table2csv.js"></script>
 </head>
 
 <body style="background-image: url('https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap_Images/background.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%;">
@@ -213,7 +213,7 @@
   </div>
 <script src="https://unpkg.com/@popperjs/core@2"></script>
 <script src="https://unpkg.com/tippy.js@6"></script>
-<script src="myScripts.js"></script>
+<script src="https://geo-classroom.github.io/group-project-frontline-gis-solutions/Webmap/myScripts.js"></script>
 
 	
 </body>
